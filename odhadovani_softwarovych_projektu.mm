9577d3ae532cce364d0880a6a4a9c9f601954425
Jiri Jagos <jiri.jagos@gmail.com>
2013-07-22 18:15:01 +0200

    adds Odhadovani softwarovych projektu

---
+++ odhadovani_softwarovych_projektu.mm
@@ -0,0 +1,34 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1374148361297" ID="ID_1347480790" MODIFIED="1374148400760" TEXT="Steve McConnel: Odhadov&#xe1;n&#xed; softwarov&#xfd;ch projekt&#x16f;">
+<node CREATED="1374148412118" ID="ID_837414792" MODIFIED="1374148426336" POSITION="right" TEXT="1. &#x10d;&#xe1;st: Rozhoduj&#xed;c&#xed; koncepce odhad&#x16f;">
+<node CREATED="1374148427625" ID="ID_1687478005" MODIFIED="1374148476104" TEXT="1. kapitola: Co je &quot;odhad&quot;">
+<node CREATED="1374148673746" ID="ID_882519097" MODIFIED="1374157868829" TEXT="Odhady, c&#xed;le a z&#xe1;vazky">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Rozli&#353;ovat mezi odhady, c&#237;li a z&#225;vazky. </b>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <b>Odhad</b>: p&#345;edpov&#283;&#271; jak dlouho bude v&#253;voj trvat (kolik bude st&#225;t)
+    </p>
+    <p>
+      <b>C&#237;l</b>: v&#253;raz &#382;&#225;douc&#237;ho obchodn&#237;ho pl&#225;nu (&quot;verzi 2.1 mus&#237;me m&#237;t hotovou na prezentaci na veletrhu v kv&#283;tnu&quot;)
+    </p>
+    <p>
+      <b>Z&#225;vazek</b>: slib dod&#225;vky definovan&#233; funk&#269;nosti na dan&#233; &#250;rovni kvality k ur&#269;it&#233;mu datu
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
