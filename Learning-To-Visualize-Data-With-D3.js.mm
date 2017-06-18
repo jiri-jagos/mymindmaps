94a7f0108ecc23865c6718714f1bcf2212c50073
Jiří Jagoš <jiri.jagos@proofreason.com>
2017-06-19 00:28:43 +0200

    Add Learning-To-Visualize-Data-With-D3.js mindmap and its working files

---
+++ Learning-To-Visualize-Data-With-D3.js.mm
@@ -0,0 +1,483 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Learning-To-Visualize-Data-With-D3.js" FOLDED="false" ID="ID_398734240" CREATED="1497719550785" MODIFIED="1497720797532" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="S.1 Introduction" POSITION="left" ID="ID_517231581" CREATED="1497720019226" MODIFIED="1497728296987" HGAP_QUANTITY="70.99999830126767 pt" VSHIFT_QUANTITY="29.999999105930353 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="L.1-3. Introduction, files download, pre-requisites and requirements" ID="ID_1987107933" CREATED="1497719952267" MODIFIED="1497817074281" HGAP_QUANTITY="46.24999903887513 pt" VSHIFT_QUANTITY="35.99999892711642 pt"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      
+    </p>
+    <ul>
+      <li>
+        contemporary web browser (SVG capable)
+      </li>
+      <li>
+        with development tools
+      </li>
+      <li>
+        Text editor
+      </li>
+      <li>
+        HTML. CSS, JavaScript knowledge
+      </li>
+      <li>
+        Webserver
+      </li>
+      <li>
+        Data to visualize
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L.4. Setting the development environment" ID="ID_1535619764" CREATED="1497720473231" MODIFIED="1497817078765" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%201/Using%20the%20project%20template/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    Import included files, some explanations and set up.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L.5. Using the project template" ID="ID_682140820" CREATED="1497721038974" MODIFIED="1497817089173"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    Further explanation of using the project template files (linked to the previous node).
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L.6. Documentation - https://github.com/d3" ID="ID_411892668" CREATED="1497727994338" MODIFIED="1497817169180" LINK="https://github.com/d3"/>
+</node>
+<node TEXT="S.2 JavaScript Essentials" POSITION="right" ID="ID_1011838643" CREATED="1497720293784" MODIFIED="1497728316650">
+<edge COLOR="#00ff00"/>
+<node TEXT="L.8. Reviewing Arrays and Objects" ID="ID_1076122758" CREATED="1497728369592" MODIFIED="1497816559101" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%202/Reviewing%20Arrays%20and%20Objects/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    Some basic explanations how to create Arrays and Objects in Javascript.
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L.9. Using The Map And Filter Array Methods" ID="ID_1164891493" CREATED="1497816534846" MODIFIED="1497817222183" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%202/Using%20map%20and%20filter%20methods/"/>
+<node TEXT="L.10. Using Special D3 Array Functions" ID="ID_1305853617" CREATED="1497817027622" MODIFIED="1497818105213" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%202/Using%20D3%20array%20functions/">
+<font BOLD="false"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      D3 has got special function for getting <b>min</b>&#160;or / and <b>max</b>&#160;values from an Array (of plain scalar values or objects):
+    </p>
+    <ul>
+      <li>
+        d3.min(array[, callback]);
+      </li>
+      <li>
+        d3..max(array[, callback]);
+      </li>
+      <li>
+        d3.extent(array[, callback])
+      </li>
+    </ul>
+    <p>
+      See the documentation or example for further explanation.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      These functions are critical to extract the data values extent to be able to scale them properly on the web page.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="S.3. Creating A Simple Bar Chart" POSITION="right" ID="ID_1508630962" CREATED="1497818139765" MODIFIED="1497821696849">
+<edge COLOR="#ff00ff"/>
+<node TEXT="L.11. Creating The SVG Element Programatically" ID="ID_7729867" CREATED="1497818183373" MODIFIED="1497819241998" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Creating%20the%20SVG%20element%20programmatically/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Description of creating an &lt;svg&gt; element using the d3 methods for adding elements:
+    </p>
+    <p>
+      
+    </p>
+    <pre><code class="javascript">// ...
+var svg = d3.select(&quot;body&quot;).append(&quot;svg&quot;)
+			.attr(&quot;id&quot;, &quot;chart&quot;)
+			.attr(&quot;width&quot;, w)
+			.attr(&quot;height&quot;, h);
+// ...</code>
+</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L.12. Creating Bars From Data" ID="ID_968702014" CREATED="1497819052941" MODIFIED="1497821677190" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Creating%20the%20bars%20from%20data/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      First description of using the .enter method to visualize the data
+    </p>
+    <p>
+      
+    </p>
+    <pre><code class="javascript">// ...
+var w = 800;
+var h = 450;
+var data = [132,71,337,93,78,43,20,16,30,8,17,21];
+var svg = d3.select(&quot;body&quot;).append(&quot;svg&quot;)
+			.attr(&quot;id&quot;, &quot;chart&quot;)
+			.attr(&quot;width&quot;, w)
+			.attr(&quot;height&quot;, h);
+svg.selectAll(&quot;.bar&quot;)
+	.data(data)
+	.enter()
+		.append(&quot;rect&quot;)
+		.attr(&quot;class&quot;, &quot;bar&quot;)
+		.attr(&quot;x&quot;, 0)
+		.attr(&quot;y&quot;, function(d,i){
+			return i * 20;
+		})
+		.attr(&quot;width&quot;, function(d,i){
+			return d;
+		})
+		.attr(&quot;height&quot;, 19)
+// ...</code>
+</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L.13. Scaling data" ID="ID_926367518" CREATED="1497821678197" MODIFIED="1497822347227" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Scaling%20the%20data/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Explanation of scaling functions used to match scales within the svg element width and height:
+    </p>
+    <p>
+      
+    </p>
+    <pre><code class="javascript">var w = 800;
+var h = 450;
+var data = [132,71,337,93,78,43,20,16,30,8,17,21];
+var x = d3.scale.linear() // this defines the scale for the x axe
+		.domain([0,d3.max(data)])
+		.range([0,w]);
+var y = d3.scale.linear() // this defines the scale for the y axe
+		.domain([0,data.length])
+		.range([0,h]);
+var svg = d3.select(&quot;body&quot;).append(&quot;svg&quot;)
+			.attr(&quot;id&quot;, &quot;chart&quot;)
+			.attr(&quot;width&quot;, w)
+			.attr(&quot;height&quot;, h);
+svg.selectAll(&quot;.bar&quot;)
+	.data(data)
+	.enter()
+		.append(&quot;rect&quot;)
+		.attr(&quot;class&quot;, &quot;bar&quot;)
+		.attr(&quot;x&quot;, 0)
+		.attr(&quot;y&quot;, function(d,i){
+			return y(i); // This returns index of the data element SCALED in regard to the domain and range
+		})
+		.attr(&quot;width&quot;, function(d,i){
+			return x(d);
+		})
+		.attr(&quot;height&quot;, function(d,i){
+			return y(1)-1;
+		})</code>
+</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L. 14. Styling The Bars With CSS" ID="ID_1228584950" CREATED="1497822037135" MODIFIED="1497822782311" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Styling%20the%20bars%20with%20CSS/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      To prevent rewriting of the element `class` attribute, use d3's `classed` method:
+    </p>
+    <p>
+      
+    </p>
+    <pre><code class="javascript">// ...
+svg.selectAll(&quot;.bar&quot;)
+	.data(data)
+	.enter()
+		.append(&quot;rect&quot;)
+		.classed(&quot;bar&quot;, true) // The required second argument determines whether to add (true) or rewrite (false) the class attribute
+		.attr(&quot;x&quot;, 0)
+		.attr(&quot;y&quot;, function(d,i){
+			return y(i);
+		})
+		.attr(&quot;width&quot;, function(d,i){
+			return x(d);
+		})
+		.attr(&quot;height&quot;, function(d,i){
+			return y(1)-1;
+		})
+// ...</code>
+</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L. 15. Adding Text To The Chart" ID="ID_198892233" CREATED="1497823142675" MODIFIED="1497824252462" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Adding%20text%20to%20the%20chart/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This lecture shows how to add test labels to the chart using d3 function `text()`. The process of adding and positioning them is literally the same (or very similar) as the process of creating the bars:
+    </p>
+    <p>
+      
+    </p>
+    <pre><code class="javascript">// ...
+svg.selectAll(&quot;.bar&quot;)
+	.data(data)
+	.enter()
+		.append(&quot;rect&quot;)
+		.classed(&quot;bar&quot;, true)
+		.attr(&quot;x&quot;, 0)
+		.attr(&quot;y&quot;, function(d,i){
+			return y(i);
+		})
+		.attr(&quot;width&quot;, function(d,i){
+			return x(d);
+		})
+		.attr(&quot;height&quot;, function(d,i){
+			return y(1)-1;
+		});
+svg.selectAll(&quot;.bar-label&quot;)
+	.data(data)
+	.enter()
+		.append(&quot;text&quot;)
+		.classed(&quot;bar-label&quot;, true)
+		.attr(&quot;x&quot;, function(d,i){
+			return x(d);
+		})
+		.attr(&quot;dx&quot;, -4) // The special attribute `dx` is the elegant way to set the label's x offset (-4px here)
+		.attr(&quot;y&quot;, function(d,i){
+			return y(i);
+		})
+		.attr(&quot;dy&quot;, function(d,i){
+			return y(1)/1.5+2;  // The special attribute `dy` is the elegant way to set the label's y offset (Dynamic value placing the label in the vertical center of the bar here)
+		})
+		.text(function(d,i){ // This sets the label's text (plain value here)
+			return d;
+		});
+// ...</code>
+</pre>
+    <p>
+      The label is horizontally &quot;anchored&quot; to the bar's right edge by setting css `text-anchor` property to `end` value:
+    </p>
+    <pre><code class="css">/* ... */
+.bar-label{
+	fill: white;
+	text-anchor:end;
+	font-size: 24px;
+}</code>
+</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L. 16. Adding Flexibility With A Chart Function" ID="ID_1089781309" CREATED="1497823182147" MODIFIED="1497824835067" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Adding%20flexibility%20with%20a%20chart%20function/"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This lecture describes how to modularize / extract actual drawing of the bar chart into the separate function which can be used independetly then. It uses a bit advanced JavaScript technique utilizing the `call` method and its possibility to set the value of the `this` object within the `called` function:
+    </p>
+    <p>
+      
+    </p>
+    <pre><code class="javascript">function plot(params){
+	this.selectAll(&quot;.bar&quot;)
+		.data(params.data)
+		.enter()
+			.append(&quot;rect&quot;)
+			.classed(&quot;bar&quot;, true)
+			.attr(&quot;x&quot;, 0)
+			.attr(&quot;y&quot;, function(d,i){
+				return y(i);
+			})
+			.attr(&quot;width&quot;, function(d,i){
+				return x(d);
+			})
+			.attr(&quot;height&quot;, function(d,i){
+				return y(1)-1;
+			});
+	this.selectAll(&quot;.bar-label&quot;)
+		.data(params.data)
+		.enter()
+			.append(&quot;text&quot;)
+			.classed(&quot;bar-label&quot;, true)
+			.attr(&quot;x&quot;, function(d,i){
+				return x(d);
+			})
+			.attr(&quot;dx&quot;, -4)
+			.attr(&quot;y&quot;, function(d,i){
+				return y(i);
+			})
+			.attr(&quot;dy&quot;, function(d,i){
+				return y(1)/1.5+2;
+			})
+			.text(function(d,i){
+				return d;
+			});
+}
+plot.call(svg,  // here we set the `svg` element as `this` within the plot function, see its declaration above
+    { // here we pass value of the `params` argument (note the key has the same name as its value and the actual `data` variable value
+	data: data
+    }
+);
+
+</code>
+</pre>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="L. 17. Using SVG Groups" ID="ID_683051886" CREATED="1497824219466" MODIFIED="1497824263701" LINK="Learning%20to%20Visualize%20Data%20with%20D3%20-%20Working%20Files/Chapter%203/Using%20SVG%20groups/"/>
+</node>
+</node>
+</map>
